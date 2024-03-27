8f5a955524ee1efd495e00126beb0b352e07c067
EmmaLiivak <114833956+EmmaLiivak@users.noreply.github.com>
2024-03-27 13:33:09 +0000

    Added function to move the paddle

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const brickContainer = document.querySelector('.brickContainer');
+const paddle = document.querySelector('.paddle');
+const gameWidth = document.querySelector('.game').offsetWidth;
+const paddleWidth = paddle.offsetWidth;
+
 const NUMBER_OF_BRICKS = 60;
+const PADDLE_STEP = 10;
 
 function generateBricks(){
   for (let i = 0; i < NUMBER_OF_BRICKS; i++){
@@ -14,3 +19,24 @@ function generateBricks(){
 }
 
 generateBricks();
+
+function movePaddle(e) {
+  let paddleLeft = paddle.offsetLeft;
+
+  switch(e.key) {
+    case 'ArrowLeft':
+      if (paddleLeft > 0) {
+        paddle.style.left = (paddleLeft - PADDLE_STEP) + 'px';
+      }
+      break;
+    case 'ArrowRight':
+      if (paddleLeft + paddleWidth < gameWidth) {
+        paddle.style.left = (paddleLeft + PADDLE_STEP) + 'px';
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+document.addEventListener('keydown', movePaddle);
