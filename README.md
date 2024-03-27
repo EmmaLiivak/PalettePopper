fc355368b523886324f08027e076f247c5f5591e
EmmaLiivak <114833956+EmmaLiivak@users.noreply.github.com>
2024-03-27 13:19:51 +0000

    Responsive canvas setup, implemented initial brick generation and added static ball and paddle

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PalettePopper
+
 Captivating brick-breaker experience where color theory takes center stage.
+
+## Resources
+
+* ['Learn JavaScript by Building 7 Games - Full Course' by freeCodeCamp.org](https://youtu.be/ec8vSKJuZTk?si=NRVQajK8dl4IUJ6z)
+* ['Creating a Brickbreaker style game in JavaScript, CSS, and HTML' by Nate McGraw](https://nmcgr001.medium.com/)
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const grid = document.querySelector('.grid');
+const brickContainer = document.querySelector('.brickContainer');
+const NUMBER_OF_BRICKS = 60;
 
-function addBrickToGrid() {
-  const brick = document.createElement('div');
-  brick.classList.add('brick');
-  grid.appendChild(brick);
+function generateBricks(){
+  for (let i = 0; i < NUMBER_OF_BRICKS; i++){
+    let brick = document.createElement('div');
+    brick.classList.add('brick'); 
+    let red = Math.floor(Math.random() * 255); 
+    let blue = Math.floor(Math.random() * 255); 
+    let green = Math.floor(Math.random() * 255); 
+    brick.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; 
+    brickContainer.appendChild(brick); 
+  }
 }
 
-addBrickToGrid();
+generateBricks();
