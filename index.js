27204f0feb33668f70331cc7e1c3cfe5a0624cc3
EmmaLiivak <114833956+EmmaLiivak@users.noreply.github.com>
2024-03-27 14:55:51 +0000

    Implemented ball movement with wall and paddle collisions

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const PADDLE_STEP = 10;
 
 const brickContainer = document.querySelector('.brickContainer');
 const paddle = document.querySelector('.paddle');
-const gameWidth = document.querySelector('.game').offsetWidth - PADDLE_STEP;
+const ball = document.querySelector('.ball');
+const game = document.querySelector('.game');
+
+const gameWidth = game.offsetWidth;
+const gameHeight = game.offsetHeight;
 const paddleWidth = paddle.offsetWidth;
+const ballDiameter = ball.offsetWidth;
+const ballRadius = ballDiameter / 2;
+let ballX = gameWidth / 2; // Initial horizontal position
+let ballY = ballRadius; // Initial vertical position
+let speedX = 3; // Horizontal speed
+let speedY = -3; // Vertical speed
 
 function generateBricks(){
   for (let i = 0; i < NUMBER_OF_BRICKS; i++){
@@ -19,6 +29,38 @@ function generateBricks(){
 }
 
 generateBricks();
+
+function moveBall() {
+  updateBallPosition();
+  checkWallCollisions();
+  checkPaddleCollision();
+}
+
+function updateBallPosition() {
+  ballX += speedX;
+  ballY += speedY;
+  ball.style.left = ballX + 'px';
+  ball.style.top = ballY + 'px';
+}
+
+function checkWallCollisions() {
+  // Collides with left or right wall
+  if (ballX > gameWidth - ballDiameter || ballX < 0) {
+    speedX = -speedX; // Reverse horizontal direction
+  }
+  // collides with top or bottom wall
+  if (ballY < 0 || ballY > gameHeight - ballDiameter) {
+    speedY = -speedY; // Reverse vertical direction
+  }
+}
+
+function checkPaddleCollision() {
+  if (ballY + ballDiameter >= gameHeight - paddle.offsetHeight &&
+    ballX >= paddle.offsetLeft && 
+    ballX <= paddle.offsetLeft + paddle.offsetWidth) {
+      speedY = -speedY; // Reverse vertical direction
+    }
+}
 
 function handleKeyPress(event) {
   switch(event.key) {
@@ -42,9 +84,17 @@ function movePaddleLeft() {
 
 function movePaddleRight() {
   const paddlePositionX = paddle.offsetLeft;
-  if (paddlePositionX + paddleWidth < gameWidth) {
+  if (paddlePositionX + paddleWidth < (gameWidth - PADDLE_STEP)) {
     paddle.style.left = (paddlePositionX + PADDLE_STEP) + 'px';
   }
 }
 
 document.addEventListener('keydown', handleKeyPress);
+
+
+function gameLoop() {
+  moveBall();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
