237c123481be5fff38ddac1591255abcbf39a8a4
EmmaLiivak <114833956+EmmaLiivak@users.noreply.github.com>
2024-05-22 11:54:44 +0000

    Updated the collision callback for collision with ball and bottom wall

--- src/entities/ballEntity.js
+++ src/entities/ballEntity.js
@@ -1,7 +1,8 @@
 import Entity from "./entityTemplate.js";
 import { CollisionComponent, PositionComponent, VelocityComponent, SizeComponent, ColorComponent, InputComponent, RenderComponent } from "../components.js";
-import { ballConfig } from "../configurations/entityConfigurations.js";
+import { ballConfig, paddleConfig } from "../configurations/entityConfigurations.js";
 import ecsSystem from "../systems/ECSSystem.js";
+import paddleEntity from "./paddleEntity.js";
 
 const ballEntity = new Entity('ball');
 ecsSystem.addEntity(ballEntity);
@@ -29,9 +30,22 @@ ballCollisionComponent.setCallback('brick', () => collisionHandler('brick'));
 
 function collisionHandler(collisionObject) {
   const velocity = ballEntity.getComponent(VelocityComponent);
+  const position = ballEntity.getComponent(PositionComponent);
 
   switch (collisionObject) {
     case 'bottomWall':
+      // Reset ball to start position and velocity
+      position.x = ballConfig.startX;
+      position.y = ballConfig.startY;
+      velocity.dx = ballConfig.startDX;
+      velocity.dy = ballConfig.startDY;
+
+      // Reset paddle to start position
+      const paddlePosition = paddleEntity.getComponent(PositionComponent);
+      paddlePosition.x = paddleConfig.startX;
+      paddlePosition.y = paddleConfig.startY;
+      
+      break;
     case 'topWall':
     case 'paddle':
     case 'brick':
