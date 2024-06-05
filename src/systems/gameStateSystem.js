ce1fe8b1b4e562e135f3a3de8f657899d043c9ae
eliivak <emma.liivak@gmail.com>
2024-06-05 12:27:41 +0300

    Implemented game end condition checks

--- src/systems/gameStateSystem.js
+++ src/systems/gameStateSystem.js
@@ -4,6 +4,8 @@ import movementSystem from './movementSystem.js'
 import inputSystem from './inputSystem.js';
 import ecsSystem from './ECSSystem.js';
 import System from "./systemTemplate.js";
+import gameStateEntity from '../entities/gameStateEntity.js';
+import { LivesComponent } from '../components.js';
 
 const pauseMenu = document.querySelector('.pause-menu');
 
@@ -14,17 +16,20 @@ class GameStateSystem extends System {
     this.lastUpdateTime = null;
   }
 
+  update() {
+    if (!this.isGameRunning) return;
+    this.checkGameEnd();
+  }
+
   startGame() {
     this.isGameRunning = true;
     this.lastUpdateTime = performance.now();
-
     gameLoop();
   }
 
   pauseGame() {
     this.isGameRunning = false;
     pauseMenu.style.display = 'flex';
-
     ecsSystem.removeSystem(movementSystem);
     inputSystem.removeComponent(paddleEntity);
   }
@@ -33,11 +38,33 @@ class GameStateSystem extends System {
     this.isGameRunning = true;
     this.lastUpdateTime = performance.now();
     pauseMenu.style.display = 'none';
-
     ecsSystem.addSystem(movementSystem);
     inputSystem.addComponent(paddleEntity);
+  }
+
+  checkGameEnd() {
+    const livesComponent = gameStateEntity.getComponent(LivesComponent);
+    if (livesComponent.lives <= 0) {
+      this.endGame(false); // Game lost
+    }
+
+    if (!ecsSystem.entities.some(entity => entity.name === 'brick')) {
+      this.endGame(true); // All bricks removed, game won
+    }
+  }
+
+  endGame(isWin) {
+    this.pauseGame();
+
+    if (isWin) {
+      console.log('Game Won!');
+    } else {
+      console.log('Game Over!');
+    }
   }
 }
 
 const gameStateSystem = new GameStateSystem();
+ecsSystem.addSystem(gameStateSystem);
+
 export default gameStateSystem;
