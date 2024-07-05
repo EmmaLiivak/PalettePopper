3d594e4d9a36429b770cdb5efc14ccd96164edb8
eliivak <emma.liivak@gmail.com>
2024-07-05 20:19:59 +0300

    Removed unnecessary imports from index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { ballEntity, gameManagerEntity, paddleEntity, walls} from './entities/index.js'
-import { collisionSystem, ecsSystem, gameStateSystem, inputSystem, movementSystem, renderingSystem } from './systems/index.js'
+import { gameManagerEntity } from './entities/index.js'
+import { ecsSystem, gameStateSystem, inputSystem } from './systems/index.js'
 import './systems/index.js';
 
 // Main game loop
